0d476da5562976272bb56fe8a40d91c613a840be
NexusRisen <bdkohn91@gmail.com>
2026-04-12 16:04:28 -0400

    Update PKHeX.Core and PKHeX.Core.AutoMod DLLs to latest source and bump version to 1.5.8

--- SysBot.Pokemon/Helpers/DudeBot.cs
+++ SysBot.Pokemon/Helpers/DudeBot.cs
@@ -2,7 +2,7 @@ namespace SysBot.Pokemon.Helpers
 {
     public static class DudeBot
     {
-        public const string Version = "v1.5.7";
+        public const string Version = "v1.5.8";
         public const string ConfigPath = "config.json";
     }
 }
