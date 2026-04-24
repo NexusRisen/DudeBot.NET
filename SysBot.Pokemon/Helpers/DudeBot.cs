cb07415c9c4d185803afdb2a62314138376547b4
NexusRisen <bdkohn91@gmail.com>
2026-04-24 19:29:34 -0400

    Bump version to 6.0.3

--- SysBot.Pokemon/Helpers/DudeBot.cs
+++ SysBot.Pokemon/Helpers/DudeBot.cs
@@ -2,7 +2,7 @@ namespace SysBot.Pokemon.Helpers
 {
     public static class DudeBot
     {
-        public const string Version = "v6.0.2";
+        public const string Version = "v6.0.3";
         public const string ConfigPath = "config.json";
     }
 }
